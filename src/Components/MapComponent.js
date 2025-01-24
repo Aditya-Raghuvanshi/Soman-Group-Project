62b50df4f7cdee3b7b305caa51c99c6770b23078
Aditya Raghuvanshi <adityaraghuvanshi159@gmail.com>
2025-01-24 14:53:51 +0530

    removed a small bug

--- src/Components/MapComponent.js
+++ src/Components/MapComponent.js
@@ -12,7 +12,7 @@ const MapComponent = () => {
             <p className='text-6xl'>Building future in <br/> <b> <i>Pune</i></b></p>
         </div>
         <div className='mt-20'>
-        <iframe className='w-full' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3782.494560926017!2d73.79136397496373!3d18.551729182547916!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc2bfd911eb8f85%3A0xdcbde4fcbad4810f!2sIndiQube%20The%20Kode!5e0!3m2!1sen!2sin!4v1737654133141!5m2!1sen!2sin" height="450" style={{border:"0" }} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        <iframe className='w-full' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3782.494560926017!2d73.79136397496373!3d18.551729182547916!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc2bfd911eb8f85%3A0xdcbde4fcbad4810f!2sIndiQube%20The%20Kode!5e0!3m2!1sen!2sin!4v1737654133141!5m2!1sen!2sin" height="450" style={{border:"0" }} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" title='Map'></iframe>
         </div>
     </div>
   )
